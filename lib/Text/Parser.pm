a7f7507baf504b752e3919eeb57e0ae3b3e598ba
Balaji Ramasubramanian <balajiram@cpan.org>
2018-12-22 19:58:02 -0800

    initial commit

---
+++ lib/Text/Parser.pm
@@ -0,0 +1,19 @@
+use warnings;
+use strict;
+package Text::Parser;
+
+# ABSTRACT: brief summary of what this module is used for
+
+use Exporter 'import';
+our (@EXPORT_OK) = ();
+our (@EXPORT)    = (@EXPORT_OK);
+
+=head1 SYNOPSIS
+
+    use Text::Parser;
+
+    ## ... code that uses your package
+
+=cut
+
+1;
---
+++ t/Text-Parser.t
@@ -0,0 +1,10 @@
+# Basic test for your module
+use strict;
+use warnings;
+
+use Test::More;
+BEGIN { use_ok 'Text::Parser'; }
+
+# Write you tests here
+
+done_testing;
