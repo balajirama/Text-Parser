579e433585f4861dbb5eeb2122dc8108a4ba3ead
Balaji Ramasubramanian <balajiram@cpan.org>
2019-03-03 17:56:17 -0800

    Create Text::Parser::AutoSplit

---
+++ lib/Text/Parser/AutoSplit.pm
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+ 
+package Text::Parser::AutoSplit;
+
+# ABSTRACT: A role that adds the ability to auto-split a line into fields
+
+use Exporter 'import';
+our (@EXPORT_OK) = ();
+our (@EXPORT) = ();
+use Moose::Role;
+
+1;
