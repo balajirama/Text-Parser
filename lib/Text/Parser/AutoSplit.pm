8dca1d4160ff81dfa656a8326a7e7ad5eb36b94b
Balaji Ramasubramanian <balajiram@cpan.org>
2019-03-03 18:25:25 -0800

    Name _fields and its traits
    Just started this using the traits

--- lib/Text/Parser/AutoSplit.pm
+++ lib/Text/Parser/AutoSplit.pm
@@ -10,4 +10,20 @@ our (@EXPORT_OK) = ();
 our (@EXPORT) = ();
 use Moose::Role;
 
+has _fields => (
+    is => 'rw', 
+    isa => 'ArrayRef[Str]', 
+    default => sub {[]}, 
+    handles => {
+        'NF' => 'count', 
+        'field' => 'get', 
+        'find_field' => 'first', 
+        'find_field_index' => 'first_index', 
+        'splice_fields' => 'splice', 
+        'line_fields' => 'elements', 
+        'field_iterator' => 'natatime',
+        'clone_fields' => 'shallow_clone', 
+    }, 
+);
+
 1;
