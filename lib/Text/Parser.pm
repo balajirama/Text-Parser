a3459dc2ec715472e942180da53029eecdba6a74
Balaji Ramasubramanian <balajiram@cpan.org>
2019-04-14 12:00:03 -0700

    Simplify code for filehandle

--- lib/Text/Parser.pm
+++ lib/Text/Parser.pm
@@ -457,8 +457,7 @@ sub filehandle {
     return if not @_ and not $self->_has_filehandle;
     $self->_save_filehandle(@_) if @_;
     $self->_clear_filename if @_;
-    my $fh = $self->_get_filehandle;
-    return $fh;
+    return $self->_get_filehandle;
 }
 
 =method lines_parsed
