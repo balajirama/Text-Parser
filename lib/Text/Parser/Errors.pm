f2ba3552c0129c007dcf4f2d776d5bf45dda1e98
Balaji Ramasubramanian <balajiram@cpan.org>
2019-04-13 21:15:34 -0700

    Changing Multiline exceptions into Throwables
    1. New error classes added.
    2. Updated code in Multiline.pm
    3. Refactored and made code nice looking
    4. Fixed all tests to pass.

--- lib/Text/Parser/Errors.pm
+++ lib/Text/Parser/Errors.pm
@@ -3,13 +3,44 @@ use strict;
 use warnings;
 
 use Throwable::SugarFactory;
+use Scalar::Util 'looks_like_number';
 
 exception 'GenericError' => 'a generic error';
 exception
-    'CantUndoMultiline' =>
-    'the parser was originally set as a multiline parser, and that cannot be undone now',
+    'CantUndoMultiline' => 'already multiline parser, cannot be undone',
+    extends             => GenericError();
+exception
+    UnexpectedEof => 'continuation character in last line, unexpected EoF',
+    has           => [
+    remaining => (
+        is  => 'ro',
+        isa => sub {
+            die "$_[0] must be a string" if '' ne ref( $_[0] );
+        }
+    )
+    ],
+    has => [
+    line_num => (
+        is  => 'ro',
+        isa => sub {
+            die "$_[0] must be a number"
+                if ref( $_[0] ) ne ''
+                or not looks_like_number( $_[0] );
+        }
+    )
+    ],
     extends => GenericError();
-
+exception
+    UnexpectedCont => 'join_last cont. character on first line',
+    has            => [
+    line => (
+        is  => 'ro',
+        isa => sub {
+            die "$_[0] must be a string" if '' ne ref( $_[0] );
+        },
+    )
+    ],
+    extends => GenericError();
 
 1;
 
