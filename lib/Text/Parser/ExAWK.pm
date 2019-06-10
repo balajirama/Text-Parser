3d22bf54f3ec098aa45aeb7d69476504b3f8e262
Balaji Ramasubramanian <balajiram@cpan.org>
2019-06-09 22:53:06 -0700

    Begin a few lines on ExAWK.pm

--- lib/Text/Parser/ExAWK.pm
+++ lib/Text/Parser/ExAWK.pm
@@ -1,9 +1,32 @@
 
 use strict;
 use warnings;
- 
+
 package Text::Parser::ExAWK;
 
 # ABSTRACT: Provides a set of subroutines to intuitively create/manage ExAWK rules
+
+use Moose;
+use MooseX::ClassAttribute;
+use Moose::Exporter;
+
+Moose::Exporter->setup_import_methods(
+    with_meta => ['rule'],
+    also      => 'Moose',
+);
+
+class_has _global_rules => (
+    is      => 'rw',
+    isa     => 'HashRef[Text::Parser::ExAWK::Rule]',
+    default => sub { {} },
+    traits  => ['Hash'],
+);
+
+class_has _class_rule_order => (
+    is      => 'rw',
+    isa     => 'HashRef[ArrayRef[Str]]',
+    default => sub { {} },
+    traits  => ['Hash'],
+);
 
 1;
