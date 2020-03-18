ee924fec4cc6d81776cd2f44999aca99beff2b66
Balaji Ramasubramanian <balajiram@cpan.org>
2020-03-17 22:37:23 -0700

    Much better documentation of the errors

--- lib/Text/Parser/Errors.pm
+++ lib/Text/Parser/Errors.pm
@@ -7,13 +7,41 @@ use Scalar::Util 'looks_like_number';
 
 # ABSTRACT: Exceptions for Text::Parser
 
+=head1 SYNOPSIS
+
+    use Text::Parser::Errors;
+    use Try::Tiny;
+    use Text::Parser;
+
+    try {
+        my $parser = Text::Parser->new();
+        # Set rules or whatever else
+        $parser->read('/path/to/file.txt');
+    } catch {
+        handle_text_parser_exceptions($_) if $_->isa(GenericError);
+    };
+
+    sub handle_text_parser_exceptions {
+        my $e = shift;
+        if ($e->isa(InvalidFilename())) { # Note function name instead of 'Text::Parser::Errors::InvalidFileName'
+            print STDERR "ERROR: No such file: ", $e->name, "\n";
+        } elsif ($e->isa(BadRuleSyntax())) {
+            print STDERR "ERROR: You rule had a problem.\nYour code: ",
+                $e->code, "\nThe stringified subroutine from it:\n",
+                $e->subroutine, "\nPerl compilation error:\n", 
+                $e->msg, "\n";
+        } # ... any more such conditions
+    }
+
 =head1 DESCRIPTION
 
-This document contains a manifest of all the exception classes thrown by L<Text::Parser>.
+L<Text::Parser> uses exceptions, like all good programming strategies. You can write an exception handler in your code and call your handler. This package contains functions that can be used to identify all the exception classes thrown by L<Text::Parser>. For example, the exception with the name C<'Text::Parser::Errors::InvalidFilename'> can be simply identified by the function C<InvalidFilename()>. Each exception class has a number of attributes, and you can access their values to print a pretty error message.
 
 =head1 EXCEPTION CLASSES
 
 All exceptions are derived from C<Text::Parser::Errors::GenericError>. They are all based on L<Throwable::SugarFactory> and so all the exception methods of those, such as C<L<error|Throwable::SugarFactory/error>>, C<L<namespace|Throwable::SugarFactory/namespace>>, etc., will be accessible. Read L<Exceptions> if you don't know about exceptions in Perl 5.
+
+Each section below names the exception class, and the function shortcut you can use in checking if the exception you caught C<isa> exception of that class. All of the exceptions here are subclasses of C<Text::Parser::Errors::GenericError> which is described below.
 
 =cut
 
@@ -29,13 +57,31 @@ sub _Num {
         or not looks_like_number( $_[0] );
 }
 
-exception 'GenericError' => 'a generic error';
+=head2 Base exception class
+
+=head3 C<Text::Parser::Errors::GenericError> - C<GenericError()>
+
+All exceptions in this package are subclasses of this exception class.
+
+=cut
+
+exception GenericError => 'the base error class for Text::Parser';
 
 =head2 Input file related errors
 
-=head3 C<Text::Parser::Errors::InvalidFilename>
+=head3 C<Text::Parser::Errors::InputFileRelated> - C<InputFileRelated()>
 
-Thrown when file name specified to C<L<read|Text::Parser/read>> or C<L<filename|Text::Parser/filename>> is invalid.
+Child of C<Text::Parser::Errors::GenericError>. All errors related to the input file come under this.
+
+=cut
+
+exception
+    InputFileRelated => 'base class for all input file related errors',
+    extends          => GenericError();
+
+=head3 C<Text::Parser::Errors::InvalidFilename> - C<InvalidFilename()>
+
+Child of C<Text::Parser::Errors::InputFileRelated>. Thrown when file name specified to C<L<read|Text::Parser/read>> or C<L<filename|Text::Parser/filename>> is invalid.
 
 =head4 Attributes
 
@@ -52,11 +98,11 @@ exception
         isa => \&_Str,
     ),
     ],
-    extends => GenericError();
+    extends => InputFileRelated();
 
-=head3 C<Text::Parser::Errors::FileNotReadable>
+=head3 C<Text::Parser::Errors::FileNotReadable> - C<FileNotReadable()>
 
-Thrown when file name specified to C<L<read|Text::Parser/read>> or C<L<filename|Text::Parser/filename>> has no read permissions or is unreadable for any other reason.
+Child of C<Text::Parser::Errors::InputFileRelated>. Thrown when file name specified to C<L<read|Text::Parser/read>> or C<L<filename|Text::Parser/filename>> has no read permissions or is unreadable for any other reason.
 
 =head4 Attributes
 
@@ -73,11 +119,11 @@ exception
         isa => \&_Str,
     ),
     ],
-    extends => GenericError();
+    extends => InputFileRelated();
 
-=head3 C<Text::Parser::Errors::FileNotPlainText>
+=head3 C<Text::Parser::Errors::FileNotPlainText> - C<FileNotPlainText()>
 
-Thrown when file name specified to C<L<read|Text::Parser/read>> or C<L<filename|Text::Parser/filename>> is not a plain text file.
+Child of C<Text::Parser::Errors::InputFileRelated>. Thrown when file name specified to C<L<read|Text::Parser/read>> or C<L<filename|Text::Parser/filename>> is not a plain text file.
 
 =head4 Attributes
 
@@ -101,13 +147,23 @@ exception
         default => undef,
     ),
     ],
-    extends => GenericError();
+    extends => InputFileRelated();
 
 =head2 Errors during line-unwrapping
 
-=head3 C<Text::Parser::Errors::BadCustomUnwrapCall>
+=head3 C<Text::Parser::Errors::LineUnwrapError> - C<LineUnwrapError()>
 
-Generated when user specifies wrong arguments to C<L<custom_line_unwrap_routines|Text::Parser/"custom_line_unwrap_routines">>.
+Child of C<Text::Parser::Errors::GenericError>. All exceptions related to line-unwrapping are grouped under this class.
+
+=cut
+
+exception
+    LineUnwrapError => 'errors related to line unwrapping',
+    extends         => GenericError();
+
+=head3 C<Text::Parser::Errors::BadCustomUnwrapCall> - C<BadCustomUnwrapCall()>
+
+Child of C<Text::Parser::Errors::LineUnwrapError>. Generated when user specifies wrong arguments to C<L<custom_line_unwrap_routines|Text::Parser/"custom_line_unwrap_routines">>.
 
 =head4 Attributes
 
@@ -120,11 +176,11 @@ exception
     BadCustomUnwrapCall =>
     'Call to custom_line_unwrap_routines was not right',
     has     => [ err => ( is => 'rw', isa => \&_Str, ), ],
-    extends => GenericError();
+    extends => LineUnwrapError();
 
-=head3 C<Text::Parser::Errors::AlreadySetLineWrapStyle>
+=head3 C<Text::Parser::Errors::AlreadySetLineWrapStyle> - C<AlreadySetLineWrapStyle()>
 
-Generated when C<L<line_wrap_style|Text::Parser/"line_wrap_style">> is set to a value other than C<custom> and yet, a call to C<L<custom_line_unwrap_routines|Text::Parser/"custom_line_unwrap_routines">> is made.
+Child of C<Text::Parser::Errors::LineUnwrapError>. Generated when C<L<line_wrap_style|Text::Parser/"line_wrap_style">> is set to a value other than C<custom> and yet, a call to C<L<custom_line_unwrap_routines|Text::Parser/"custom_line_unwrap_routines">> is made.
 
 =head4 Attributes
 
@@ -137,11 +193,11 @@ exception
     AlreadySetLineWrapStyle =>
     'Called custom_line_unwrap_routines even though line_wrap_style is defined and not custom',
     has     => [ value => ( is => 'ro', isa => \&_Str ) ],
-    extends => GenericError();
+    extends => LineUnwrapError();
 
-=head3 C<Text::Parser::Errors::UndefLineUnwrapRoutine>
+=head3 C<Text::Parser::Errors::UndefLineUnwrapRoutine> - C<UndefLineUnwrapRoutine()>
 
-Generated when user forgets to set a custom line-unwrap routine, but says that the C<line_wrap_style> is C<custom>.
+Child of C<Text::Parser::Errors::LineUnwrapError>. Generated when user forgets to set a custom line-unwrap routine, but says that the C<line_wrap_style> is C<custom>.
 
 =head4 Attributes
 
@@ -153,11 +209,11 @@ Generated when user forgets to set a custom line-unwrap routine, but says that t
 exception
     UndefLineUnwrapRoutine => 'Forgot to set custom line-unwrap routines?',
     has                    => [ name => ( is => 'rw', isa => \&_Str, ) ],
-    extends                => GenericError();
+    extends                => LineUnwrapError();
 
-=head3 C<Text::Parser::Errors::UnexpectedEof>
+=head3 C<Text::Parser::Errors::UnexpectedEof> - C<UnexpectedEof()>
 
-Thrown when a line continuation character indicates that the last line in the file is wrapped on to the next line.
+Child of C<Text::Parser::Errors::UnexpectedEof>. Thrown when a line continuation character indicates that the last line in the file is wrapped on to the next line.
 
 =head4 Attributes
 
@@ -181,11 +237,11 @@ exception
         isa => \&_Num,
     ),
     ],
-    extends => GenericError();
+    extends => LineUnwrapError();
 
-=head3 C<Text::Parser::Errors::UnexpectedCont>
+=head3 C<Text::Parser::Errors::UnexpectedCont> - C<UnexpectedCont()>
 
-Thrown when a line continuation character on the first line indicates that it is a continuation of a previous line.
+Child of C<Text::Parser::Errors::UnexpectedCont>. Thrown when a line continuation character on the first line indicates that it is a continuation of a previous line.
 
 =head4 Attributes
 
@@ -202,21 +258,23 @@ exception
         isa => \&_Str,
     ),
     ],
-    extends => GenericError();
+    extends => LineUnwrapError();
 
 =head2 ExAWK rule syntax related
 
-=head3 C<Text::Parser::Errors::ExAWK>
+=head3 C<Text::Parser::Errors::ExAWK> - C<ExAWK()>
 
-All errors corresponding to the L<Text::Parser::Rule> class.
+Child of C<Text::Parser::Errors::GenericError>. All errors corresponding to the L<Text::Parser::Rule> class.
 
 =cut
 
-exception ExAWK => 'a class of errors', extends => GenericError();
+exception
+    ExAWK   => 'a class of errors pertaining to any issues in the rules',
+    extends => GenericError();
 
-=head3 C<Text::Parser::Errors::BadRuleSyntax>
+=head3 C<Text::Parser::Errors::BadRuleSyntax> - C<BadRuleSyntax()>
 
-Generated from L<Text::Parser::Rule> class constructor or from the accessors of C<condition>, C<action>, or the method C<add_precondition>, when the rule strings specified fail to compile properly.
+Child of C<Text::Parser::Errors::ExAWK>. Generated from L<Text::Parser::Rule> class constructor or from the accessors of C<condition>, C<action>, or the method C<add_precondition>, when the rule strings specified fail to compile properly.
 
 =head4 Attributes
 
@@ -249,9 +307,9 @@ exception
     ],
     extends => ExAWK();
 
-=head3 C<Text::Parser::Errors::IllegalRuleNoIfNoAct>
+=head3 C<Text::Parser::Errors::IllegalRuleNoIfNoAct> - C<IllegalRuleNoIfNoAct()>
 
-Generated from constructor of the L<Text::Parser::Rule> when the rule is created with neither a C<condition> nor an C<action>
+Child of C<Text::Parser::Errors::ExAWK>. Generated from constructor of the L<Text::Parser::Rule> when the rule is created with neither a C<condition> nor an C<action>
 
 =cut
 
@@ -259,9 +317,9 @@ exception
     IllegalRuleNoIfNoAct => 'Rule created without required components',
     extends              => ExAWK();
 
-=head3 C<Text::Parser::Errors::IllegalRuleCont>
+=head3 C<Text::Parser::Errors::IllegalRuleCont> - C<IllegalRuleCont()>
 
-Generated when the rule option C<continue_to_next> of the L<Text::Parser::Rule> object is set true when C<dont_record> is false.
+Child of C<Text::Parser::Errors::ExAWK>. Generated when the rule option C<continue_to_next> of the L<Text::Parser::Rule> object is set true when C<dont_record> is false.
 
 =cut
 
@@ -270,9 +328,9 @@ exception
     'Rule cannot continue to next if action result is recorded',
     extends => ExAWK();
 
-=head3 C<Text::Parser::Errors::RuleRunImproperly>
+=head3 C<Text::Parser::Errors::RuleRunImproperly> - C<RuleRunImproperly()>
 
-Generated from C<run> method of L<Text::Parser::Rule> is called without an object of L<Text::Parser> as argument.
+Child of C<Text::Parser::Errors::ExAWK>. Generated from C<run> method of L<Text::Parser::Rule> is called without an object of L<Text::Parser> as argument.
 
 =cut
 
@@ -282,9 +340,9 @@ exception
 
 =head2 Related to class rulespec
 
-=head3 C<Text::Parser::Errors::RuleSpecError>
+=head3 C<Text::Parser::Errors::RuleSpecError> - C<RuleSpecError()>
 
-Base class for all RuleSpec errors, generated using the functions of L<Text::Parser::RuleSpec> incorrectly.
+Child of C<Text::Parser::Errors::GenericError>. Base class for all RuleSpec errors, generated using the functions of L<Text::Parser::RuleSpec> incorrectly.
 
 =cut
 
@@ -292,9 +350,9 @@ exception
     RuleSpecError => 'base class for errors from Text::Parser::RuleSpec',
     extends       => GenericError();
 
-=head3 C<Text::Parser::Errors::SpecMustHaveName>
+=head3 C<Text::Parser::Errors::SpecMustHaveName> - C<SpecMustHaveName()>
 
-Thrown when C<L<applies_rule|Text::Parser::RuleSpec/applies_rule>> is called without a rule name.
+Child of C<Text::Parser::Errors::RuleSpecError>. Thrown when C<L<applies_rule|Text::Parser::RuleSpec/applies_rule>> is called without a rule name.
 
 =head4 Attributes
 
@@ -313,9 +371,9 @@ exception
     ),
     ];
 
-=head3 C<Test::Parser::Errors::SpecRequiresHash>
+=head3 C<Test::Parser::Errors::SpecRequiresHash> - C<SpecRequiresHash()>
 
-Thrown when C<L<applies_rule|Text::Parser::RuleSpec/applies_rule>> is called with invalid number of options.
+Child of C<Text::Parser::Errors::RuleSpecError>. Thrown when C<L<applies_rule|Text::Parser::RuleSpec/applies_rule>> is called with invalid number of options.
 
 =head4 Attributes
 
@@ -335,9 +393,9 @@ exception
     ),
     ];
 
-=head3 C<Text::Parser::Errors::MainCantApplyRule>
+=head3 C<Text::Parser::Errors::MainCantApplyRule> - C<MainCantApplyRule()>
 
-This error means that C<L<applies_rule|Text::Parser::RuleSpec/applies_rule>> was called from your C<main> program (which is not right).
+Child of C<Text::Parser::Errors::MainCantApplyRule>. This error means that C<L<applies_rule|Text::Parser::RuleSpec/applies_rule>> was called from your C<main> program (which is not right).
 
 =head4 Attributes
 
@@ -356,9 +414,9 @@ exception
     ),
     ];
 
-=head3 C<Text::Parser::Errors::MainCantUnwrapLines>
+=head3 C<Text::Parser::Errors::MainCantUnwrapLines> - C<MainCantUnwrapLines()>
 
-This means C<L<unwraps_lines_using|Text::Parser::RuleSpec/"unwraps_lines_using">> was called from C<main> and that is not right.
+Child of C<Text::Parser::Errors::MainCantUnwrapLines>. This means C<L<unwraps_lines_using|Text::Parser::RuleSpec/"unwraps_lines_using">> was called from C<main> and that is not right.
 
 =cut
 
@@ -366,28 +424,15 @@ exception
     MainCantUnwrapLines => 'unwraps_lines_using was called in main',
     extends             => RuleSpecError();
 
-=head3 C<Text::Parser::Errors::NameRuleUniquely>
+=head3 C<Text::Parser::Errors::NameRuleUniquely> - C<NameRuleUniquely()>
 
-If the same rule name is used in the same namespace, this error is thrown.
+Child of C<Text::Parser::Errors::NameRuleUniquely>. If the same rule name is used in the same namespace, this error is thrown.
 
 =cut
 
 exception
     NameRuleUniquely => 'name rules uniquely',
     extends          => RuleSpecError();
-
-=head2 Miscellaneous
-
-=head3 C<Text::Parser::Errors::SingleParamsToNewMustBeHashRef>
-
-This error is thrown in place of C<Moose::Exception::SingleParamsToNewMustBeHashRef>.
-
-=cut
-
-exception
-    SingleParamsToNewMustBeHashRef =>
-    'single argument to new() must be hashref',
-    extends => GenericError();
 
 =head1 SEE ALSO
 
