3b873dee10c20cb003793fb5719f47c813d4c251
Balaji Ramasubramanian <balaji.ramasubramanian@gmail.com>
2020-11-25 18:07:46 -0800

    Documentation added to Text::Parser::Error

--- lib/Text/Parser/Error.pm
+++ lib/Text/Parser/Error.pm
@@ -8,16 +8,38 @@ use Moose;
 use Moose::Exporter;
 extends 'Throwable::Error';
 
+Moose::Exporter->setup_import_methods( as_is => ['parser_exception'], );
+
 =head1 DESCRIPTION
 
-=cut
+This class replaces the older C<Text::Parser::Errors> which created hundreds of subclasses. That method seemed very counter-productive and difficult to handle for programmers. There is only one function in this class that is used inside the L<Text::Parser> package. 
 
-Moose::Exporter->setup_import_methods( as_is => ['parser_exception'], );
+Any exceptions thrown by this package will be an instance of L<Text::Parser::Error>. And C<Text::Parser::Error> is a subclass of C<Throwable::Error>. So you can write your code like this:
+
+    use Try::Tiny;
+
+    try {
+        my $parser = Text::Parser->new();
+        # do something
+        $parser->read();
+    } catch {
+        print $_->as_string, "\n" if $_->isa('Text::Parser::Error');
+    };
+
+=head1 FUNCTIONS
+
+=func parser_exception
+
+Accepts a single string argument and uses it as the message attribute for the exception thrown.
+
+    parser_exception("Something bad happened") if $something_bad;
+
+=cut
 
 sub parser_exception {
     my $str = shift;
     $str = 'Unknown error from ' . caller() if not defined $str;
-    Text::Parser::Error->throw(message => $str);
+    Text::Parser::Error->throw( message => $str );
 }
 
 1;
